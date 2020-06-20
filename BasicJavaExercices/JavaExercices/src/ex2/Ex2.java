bc1d335d4336dc5ee0da9438b7550b77a53e04c8
Ionut <ionutputinicabz@gmail.com>
2020-06-20 14:44:55 +0200

    Added Ex2

---
+++ BasicJavaExercices/JavaExercices/src/ex2/Ex2.java
@@ -0,0 +1,11 @@
+package ex2;
+
+public class Ex2 {
+	public static void main(String args[]) {
+		int a=74;
+		int b=36;
+		int sum=a+b;
+		System.out.println("The sum of the two numbers is = "+ sum);
+	}
+
+}
