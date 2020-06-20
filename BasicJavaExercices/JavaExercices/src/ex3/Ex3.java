ebd4f652f849e2c506207acfda4e13081d96cdd5
Ionut <ionutputinicabz@gmail.com>
2020-06-20 14:47:51 +0200

    Added Ex3

---
+++ BasicJavaExercices/JavaExercices/src/ex3/Ex3.java
@@ -0,0 +1,10 @@
+package ex3;
+
+public class Ex3 {
+	public static void main(String args[]) {
+	int a=50, b=3;
+	float division = a/b;
+	System.out.println("The division of a and b is = "+ division);
+	}
+
+}
