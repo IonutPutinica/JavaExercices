95ae72c7d29d9bad29eb0e6646f078b5d560b5b4
Ionut <ionutputinicabz@gmail.com>
2020-06-20 14:52:18 +0200

    Added Ex4

---
+++ BasicJavaExercices/JavaExercices/src/ex4/Ex4.java
@@ -0,0 +1,20 @@
+package ex4;
+//print the result of the following operations
+//-5 + 8 * 6
+//(55+9) % 9
+//20 + -3*5 / 8
+//5 + 15 / 3 * 2 - 8 % 3
+
+public class Ex4 {
+	public static void main(String args[])
+	{
+		float a = -5+8*6;
+		float b = (55+9)%9;
+		float c = 20+ -3*5/8;
+		float d = 5+15/3*2-8%3;
+		System.out.println("a = "+a);
+		System.out.println("b = "+b);
+		System.out.println("c = "+c);
+		System.out.println("d = "+d);
+	}
+}
